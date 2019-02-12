77276d73879cb3ba6510bd327700bae1e433b55a
Jennifer Kim <jkim8191@gmail.com>
2019-02-12 17:43:16 +0000

    Automatically backed up by Learn

---
+++ worlds_best_restaurant/lib/worlds_best_restaurant.rb
@@ -0,0 +1,6 @@
+require "worlds_best_restaurant/version"
+
+module WorldsBestRestaurant
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ worlds_best_restaurant/lib/worlds_best_restaurant/version.rb
@@ -0,0 +1,3 @@
+module WorldsBestRestaurant
+  VERSION = "0.1.0"
+end
